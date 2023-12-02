e2d48b2752aa2b97b3e23275714cae129c7797c1
malek12020 <malekjoseph2020@gmail.com>
2023-12-02 19:54:09 +0200

    started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,29 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "red",
+        secondary: {
+          100: "blue",
+          200: "yellow",
+        },
+      },
+      fontFamily: {
+        fancy: ["Roboto Mono"],
       },
     },
   },
   plugins: [],
-}
+};
 export default config
