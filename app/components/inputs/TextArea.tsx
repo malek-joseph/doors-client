b1e79666d416fc1800e6020ff27ad11b1722e53f
malek12020 <malekjoseph2020@gmail.com>
2024-06-28 17:51:40 +0300

    deploy fix

--- app/components/inputs/TextArea.tsx
+++ app/components/inputs/TextArea.tsx
@@ -9,9 +9,14 @@ interface TextAreaProps {
 }
 
 const TextArea: React.FC<TextAreaProps> = ({ label, value, placeholder, onChange }) => {
-    const handleContextMenu = (e: React.MouseEvent<HTMLInputElement>) => {
-    e.preventDefault(); 
+  const handleContextMenu = (e: React.MouseEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
+  };
+
   return (
     <div className="flex flex-col w-full">
       <label className="mb-2 text-lg font-medium text-gray-700">{label}</label>
@@ -19,8 +24,8 @@ const TextArea: React.FC<TextAreaProps> = ({ label, value, placeholder, onChange
         className="w-full p-4 border-2 min-h-[250px] border-gray-300 rounded-lg focus:outline-none focus:border-teal-500"
         placeholder={placeholder}
         value={value}
-          onContextMenu={handleContextMenu} 
-        onChange={(e) => onChange(e.target.value)}
+        onContextMenu={handleContextMenu}
+        onChange={handleChange}
       />
     </div>
   );
